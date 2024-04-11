010e6cb1f9ba94973b2e7f723e49594e17f27b45
mb1721 <masonbennett1721@gmail.com>
2024-04-11 00:52:39 -0400

    Add filter for page navs

--- site/views/site/src/App.js
+++ site/views/site/src/App.js
@@ -6,22 +6,34 @@ import KeyList from '../../../assets/scripts/view-utils/key-list';
 import * as PAGES from './pages';
 
 export default function App() {
-  const routeKeys = new KeyList();
-  const pageList = Object.keys(PAGES).map(pageName => pageName.toLowerCase());
-  const pagesArr = Object.values(PAGES);
+  // filter page navs in layout header
+  const pageNavList = Object.keys(PAGES).map(pageName => pageName.toLowerCase())
+    .filter(pageName => !/(error(404)?|home|index|contact)/.test(pageName));
 
-  // create routes from PAGES modules 
+  // create routes from PAGES modules
+  const pagesArr = Object.values(PAGES); // array of page modules
+  const routeKeys = new KeyList(); // initialize route keylist
+
   const routes = (() => {
     return pagesArr.map(Page => { // access page module
-      const { name } = Page;
+      const name = Page.name.toLocaleLowerCase();
       const newKey = routeKeys.generateKey(name); // generate key
-      return <Route key={newKey} path={name} element={<Page />}/>;
+      
+      switch (name) {
+        case 'error':
+        case 'error404':
+          return <Route key={newKey} path="*" element={<Page />}/>;
+        case 'home':
+          return <Route key={newKey} path="/" element={<Page />}/>;
+        default:
+          return <Route key={newKey} path={name} element={<Page />}/>;
+      }
     }); 
   })();
 
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<Layout pageList={pageList} />}>
+      <Route path="/" element={<Layout pageList={pageNavList} />}>
         {routes}
       </Route>
     )
