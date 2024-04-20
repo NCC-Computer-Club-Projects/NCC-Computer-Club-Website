682013d9066c17324bac3743b62f1ae3e9ddafd3
mb1721 <masonbennett1721@gmail.com>
2024-04-20 18:07:44 -0400

    Add header styles

--- n4c/views/site/src/components/Header/Header.js
+++ n4c/views/site/src/components/Header/Header.js
@@ -1,18 +1,24 @@
 import './Header.scss';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { LayoutContext } from '../../contexts';
 import KeyList from '../../../../../assets/scripts/view-utils/key-list';
 import brand from '../../../../../assets/images/logo/n4c-logo.svg';
 
 export default function Header() {
+  
+
   const pageList = useContext(LayoutContext);
   const keyList = new KeyList();
+
+  const selectLink = (e) => {
+    
+  }
   
   const linkComponents = pageList.map(page => {
     const linkName = page.replace(page[0], page[0].toUpperCase());
     return (
-      <li className="nav-item" key={keyList.generateKey(page)}>
+      <li className="nav-item d-inline-flex align-items-center" key={keyList.generateKey(page)} onClick={selectLink}>
         <Link to={page === 'home' ? '/' : page} className="nav-link">
           {/pcrepair/i.test(linkName) ? 'PcRepair' : linkName}
         </Link>
@@ -23,10 +29,11 @@ export default function Header() {
 
   return (
     <header id="Header">
-      <nav className="navbar navbar-expand-md bg-primary">
-        <div id="pages" className="container-fluid">
-          <Link className="navbar-brand" to="/" >
-            <img src={brand} height={100}/>
+      <nav className="navbar navbar-expand-md mx-3">
+        <div className="container-fluid justify-content-around align-items-md-stretch">
+          <Link className="navbar-brand d-inline-flex flex-row align-items-center gap-3" to="/" >
+            <img src={brand} height={80} className="rounded-circle"/>
+            <h1 className='m-0'>N4C</h1>
           </Link>
           <button 
             className="navbar-toggler" 
@@ -38,8 +45,8 @@ export default function Header() {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="pages">
-            <ul className="navbar-nav">
+          <div className="collapse navbar-collapse justify-content-around" id="pages">
+            <ul className="navbar-nav justify-content-evenly gap-5 h-100">
               {linkComponents}
             </ul>
           </div>
