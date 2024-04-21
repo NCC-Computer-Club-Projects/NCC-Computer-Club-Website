04b8795e4cbc5394531f5b9d0f748178a46b0cd9
mb1721 <masonbennett1721@gmail.com>
2024-04-20 22:43:33 -0400

    Fix styling

--- n4c/views/site/src/pages/Home.js
+++ n4c/views/site/src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '@styles/pages/Home.scss';
+import heroImg from '@images/fun-at-primantis.jpg';
 
 export default function Home() {
   return (
-    <div id="Home">
-      
+    <div id='Home'>
+      <div className='hero container-fluid'>
+        <img className='img-fluid' src={heroImg} alt='Computer Club members hanging out at Primanti Bros'/>
+      </div>
     </div>
   );
 }
