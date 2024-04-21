eb7c34c895cee8529f6b4d177cf6ff8b205112ef
mb1721 <masonbennett1721@gmail.com>
2024-04-21 15:48:53 -0400

    Adjust link styles

--- n4c/views/site/src/components/Banner/Banner.js
+++ n4c/views/site/src/components/Banner/Banner.js
@@ -7,8 +7,9 @@ export default function Banner({header, main, action}) {
       <h1>{header}</h1>
       <p>{main}</p>
       <div className='action d-flex justify-content-end'>
-        <a href={action.link} target='_blank'>
-          <h3>{action.text} {action.arrow && '\u2192'}</h3>
+        <a href={action.link} target='_blank' className='action-text d-inline-flex flex-row'>
+          <h3>{action.text}</h3>
+          <h3>{action.arrow && '\u2192'}</h3>
         </a>
       </div>
     </div>
