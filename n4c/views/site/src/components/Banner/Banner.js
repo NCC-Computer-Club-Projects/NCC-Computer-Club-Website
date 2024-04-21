30cf8cb9d85ea97c0eaa99f968bb0f41b9898570
mb1721 <masonbennett1721@gmail.com>
2024-04-21 19:04:15 -0400

    Add mission section

--- n4c/views/site/src/components/Banner/Banner.js
+++ n4c/views/site/src/components/Banner/Banner.js
@@ -8,8 +8,8 @@ export default function Banner({header, main, action}) {
       <p>{main}</p>
       <div className='action d-flex justify-content-end'>
         <a href={action.link} target='_blank' className='action-text d-inline-flex flex-row'>
-          <h3>{action.text}</h3>
-          <h3>{action.arrow && '\u2192'}</h3>
+          <h4>{action.text}</h4>
+          <h4>{action.arrow && '\u2192'}</h4>
         </a>
       </div>
     </div>
--- n4c/views/site/src/pages/Home.js
+++ n4c/views/site/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '@styles/pages/Home.scss';
 import Banner from '../components/Banner/Banner';
+import missionImg from '@images/mock-images/mission.jpg'
 
 export default function Home() {
   return (
     <div id='Home'>
-      <div className='hero container-fluid position-relative p-0'>
+      <section className='hero container-fluid position-relative p-0'>
         <Banner 
           header='Be At Skills USA' 
           main='Support N4C as we take on SkillsUSA! Our members will compete in web design and cyber security.' 
@@ -22,7 +23,14 @@ export default function Home() {
             12pm - 4pm<br/>
           </h1>
         </div>
-      </div>
+      </section>
+      <section className='mission d-flex flex-row'>
+        <div className='mission-text'>
+          <h3>Our Mission</h3>
+          <p>Students of Northampton's computer and information technology sector learn many skills in their studies, but putting those knowledge into practice can be a challenge. Our members posses the potential to create something big for themselves and the community using not only what they learn here at Northampton, but also the skills they acquire through their own research and hard work, Northampton Community College Computer Club– or N4C–  gives students a chance to put their learning into practice!</p>
+        </div>
+        <img className='mission-img rounded-circle' src={missionImg}/>
+      </section>
     </div>
   );
 }
