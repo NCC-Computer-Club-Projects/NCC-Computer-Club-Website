d1f20fbbfdf7513611727e9cd07960adbcd7d349
mb1721 <masonbennett1721@gmail.com>
2024-04-22 22:11:16 -0400

    Add more footer styles

--- n4c/views/site/src/components/Footer/Footer.js
+++ n4c/views/site/src/components/Footer/Footer.js
@@ -55,7 +55,10 @@ export default function Footer() {
     <footer className='footer'>
       <div className='footer-top'>
         <div className='logo-space'>
-          <img src={logo} className='logo-space-logo rounded-circle'/>
+          <div className='logo-space-logo d-inline-flex align-items-center'>
+            <img src={logo} className='rounded-circle'/>
+            <h1 className='m-0'>N4C</h1>
+          </div>
           <div className='location'>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-geo-alt-fill" viewBox="0 0 16 16">
               <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/>
