b5f5871d9f766785638d622096fedddec3019c65
mb1721 <masonbennett1721@gmail.com>
2024-05-10 10:51:53 -0400

    Use strict mode

--- n4c/views/site/src/index.tsx
+++ n4c/views/site/src/index.tsx
@@ -4,4 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
-root.render(<App/>);
+root.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+);
